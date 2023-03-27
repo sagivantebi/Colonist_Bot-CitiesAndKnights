2fd3bd45eaebb6b7704763dd49f77dff18946243
Sagiv_Antebi <sagiva13@gmail.com>
2023-03-27 20:18:57 +0300

    All Project

---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+Creator - Sagiv Antebi
+"""
+from selenium import webdriver
+from time import sleep
+
+"""
+* class name: MainBot
+* class Operation: main class of the bot
+"""
+class MainBot():
+    def __init__(self):
+        #The chrome web driver path
+        self.driver = webdriver.Chrome('C:\\Users\\sagiv\\Desktop\\CandK_Colonist\\chromedriver')
+        print("\n-------------------------------------------------------------")
+        print("""\nＳａｇｉｖ Ａｎｔｅｂｉ － Ａｌｌ Ｒｉｇｈｔｓ Ｒｅｓｅｒｖｅｄ\n""")
+        print("-------------------------------------------------------------\n")
+
+    """
+    * Function name: startDownload
+    * Function Operation: start the download process
+    """
+    def startSearch(self,url):
+        # create action chain object
+        self.driver.get(url)
+        lobby_btn = self.driver.find_element("xpath", "/html/body/header/div[1]/div[1]/a[2]")
+        sleep(2)
+        lobby_btn.click()
+        lobby_btn.click()
+        lobby_btn.click()
+        lobby_btn.click()
+        lobby_btn.click()
+        sleep(2)
+        refresh_btn = self.driver.find_element("xpath","/html/body/div[2]/div[5]/div[2]/div[2]/div[2]/div/table[1]/thead/tr/th[4]/img")
+
+        first_row_name = "None"
+        sleep(1)
+        while self.driver.current_url == url:
+            sleep(1)
+            try:
+                refresh_btn.click()
+                refresh_btn.click()
+                sleep(1)
+                first_row_name = self.driver.find_element("xpath","/html/body/div[2]/div[5]/div[2]/div[2]/div[2]/div/table[1]/tbody/tr[1]/td[1]").text
+                if "Cities & Knights" in first_row_name:
+                    first_row_btn = self.driver.find_element("xpath","/html/body/div[2]/div[5]/div[2]/div[2]/div[2]/div/table[1]/tbody/tr[1]")
+                    first_row_btn.click()
+                    first_row_btn.click()
+                    sleep(2)
+                    submit_btn = self.driver.find_element("xpath","/html/body/div[2]/div[5]/div[3]/div[2]/div[3]/div[1]/input")
+                    submit_btn.click()
+                    sleep(8)
+            except Exception:
+                first_row_name = "None"
+
+
+
+
+def main():
+    #creating copy of the bot
+    bot = MainBot()
+    #start command to search
+    bot.startSearch('https://colonist.io/')
+    print("""
+██████╗░░█████╗░███╗░░██╗███████╗██╗
+██╔══██╗██╔══██╗████╗░██║██╔════╝██║
+██║░░██║██║░░██║██╔██╗██║█████╗░░██║
+██║░░██║██║░░██║██║╚████║██╔══╝░░╚═╝
+██████╔╝╚█████╔╝██║░╚███║███████╗██╗
+╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝╚═╝""")
+    print("\n-------------------------------------------------------------")
+    print("""\nＳａｇｉｖ Ａｎｔｅｂｉ － Ａｌｌ Ｒｉｇｈｔｓ Ｒｅｓｅｒｖｅｄ\n""")
+    print("-------------------------------------------------------------\n")
+    sleep(50)
+    input("Press enter to exit ;)")
+
+if __name__ == "__main__":
+    main()
